ad843c6bd49ccc33dd0d1df74a34752cc50a8888
Francisco Viana <chicoviana12@hotmail.com>
2023-12-16 21:49:25 -0300

    v2 tested and images saved, model saved have lost shared memories in case of retrain

--- src/models.py
+++ src/models.py
@@ -7,7 +7,11 @@ def gen_model_v1(input_shape=(None, 3 + 2)):
     Basic hard-coded model for 3 step forecast, first try using an tanh activation
     before the usage of LSTM, as to simulate how the LSTMCell sees a information
     input, but such assumption was incorrect due to the usage of sigmoid internally
-    which reduces the value of 'raw' tanh.
+    the LSTMCell which reduces the value of 'raw' tanh, rendering the artificial
+    tanh input wrong.
+    
+    Also, when training both value and probabilities, the backpropagation for
+    the probabilities should be restricted only to the last Dense trainable_variables.
     
     Returns model that takes (N, channels) and outputs [(N + 3, 1), (N + 3, 2)]
     which are values and categorical, so it can be checked the amount of rain
@@ -55,3 +59,59 @@ def gen_model_v1(input_shape=(None, 3 + 2)):
     model = models.Model(_in, [out_value, out_prob])
     return model
     
+    
+def gen_model_v2(input_shape=(None, 3 + 2)):
+    '''
+
+        
+    '''
+    
+    lstm_cell = layers.LSTMCell(64)
+    lstm_rnn  = layers.RNN(lstm_cell, return_sequences=True, return_state=True)
+
+    _in = layers.Input(input_shape)
+    c = layers.BatchNormalization()(_in)
+
+    # abstração inicial - "Xception-like" - original com Conv2D e SeparableConv2D
+    c = layers.Dense(32)(c)
+    c = layers.BatchNormalization()(c)
+    c = layers.Activation('relu')(c)
+
+    c = layers.Dense(64)(c)
+    c = layers.BatchNormalization()(c)
+    c = layers.Activation('relu')(c)
+
+    for _ in range(8):
+        res = c
+        
+        for _ in range(3):
+            c = layers.Activation('relu')(c)
+            c = layers.Dense(64)(c)
+            c = layers.BatchNormalization()(c)
+            
+        c = layers.Add()([c, res])
+
+    # LSTM convencional, para a próxima camada conseguir usar os inputs
+    # de forma consistente "tanh * sigmoid" nos steps futuros
+    c = layers.LSTM(64, return_sequences=True)(c)
+    out0, *state_warm = lstm_rnn(c)
+
+    out1, state_out1 = lstm_cell(out0[:, -1], states=state_warm)
+    out2, state_out2 = lstm_cell(out1, states=state_out1)
+    out3, _          = lstm_cell(out2, states=state_out2)
+
+    pred_outs = tf.stack([out1, out2, out3], axis=1)
+    outs = layers.Concatenate(axis=1)([out0, pred_outs])
+
+    out_value = layers.Dense(128)(outs)
+    out_value = layers.BatchNormalization()(out_value)
+    out_value = layers.Activation('relu')(out_value)
+    out_value = layers.Dense(1, name='value')(out_value)
+    
+    out_prob = layers.Dense(128)(outs)
+    out_prob = layers.BatchNormalization()(out_prob)
+    out_prob = layers.Activation('relu')(out_prob)
+    out_prob  = layers.Dense(2, name='prob')(out_prob)
+
+    model = models.Model(_in, [out_value, out_prob])
+    return model
--- train.py
+++ train.py
@@ -3,20 +3,24 @@ import src.losses as losses
 import src.dataset as dataset
 import src.models as models
 
-x_train, y_train, x_test, y_test, weights, times_train, times_test = dataset.load_data()
+x_train, y_train, x_test, y_test, weights, times_train, times_test = dataset.load_data_v2()
 
-model = models.gen_model((None, x_train.shape[-1]))
+model = models.gen_model_v2((None, x_train.shape[-1]))
 model.compile('adam', ['mse', losses.WeightedSCCE(weights, from_logits=True)])
 
 model.fit(
     x_train, 
-    y_train, 
-    epochs=100, 
-    validation_split=0.2, 
+    y_train,
+    epochs=100,
+    validation_split=0.2,
     callbacks=[
-        callbacks.Plot(x_train[:10], y_train[:10], times_train[:10], 'train'), 
-        callbacks.Plot(x_test[:10], y_test[:10], times_test[:10], 'test')
+        callbacks.Plot(x_train[:20], [y_train[0][:20], y_train[1][:20]], times_train[:20], 'train'), 
+        callbacks.Plot(x_train[365:365+20], [y_train[0][365:365+20], y_train[1][365:365+20]], times_train[365:365+20], 'train2'), 
+        callbacks.Plot(x_test[:20], [y_test[0][:20], y_test[1][:20]], times_test[:20], 'test'),
+        callbacks.Plot(x_test[365:365+20], [y_test[0][365:365+20], y_test[1][365:365+20]], times_test[365:365+20], 'test2'),
     ]
 )
 
 model.evaluate(x_test, y_test)
+
+model.save('models/model_v2.h5')
