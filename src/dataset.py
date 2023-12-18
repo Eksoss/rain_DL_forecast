0490b3d013b009ee0e30ab0352a65ef8d5bd83ec
Francisco Viana <chicoviana12@hotmail.com>
2023-12-18 01:31:06 -0300

    era5 and goes data load added

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,12 @@
+import glob
+
 import numpy as np
 import pandas as pd
+
+import netCDF4 as nc
+from ncBuilder import ncHelper
+from tqdm import tqdm
+from scipy.interpolate import RectBivariateSpline
 
 def e(Td):
     return 6.112 * np.exp((17.67 * Td) / (Td + 243.5))
@@ -285,3 +292,273 @@ def load_data_v2(path='data/sp_mirante.csv'):
     ]
     
     return x_train, [y_train, y_train_bin], x_test, [y_test, y_test_bin], weights, times_train, times_test
+
+
+def load_base_v3(path):
+    '''
+    D*
+    
+    Returns inputs, outputs and times, all prepared with channels, but not sample
+    dimension
+    
+    Parameters
+    ----------
+    path : str
+        String that contains the path to the .csv file.
+        
+    Returns
+    -------
+    inputs : np.ndarray
+        Array with (n_steps, channels), being the n_steps the days of avaiable data and
+        channels the number of variables used as input.
+    outputs : np.ndarray
+        Array with (n_steps, channels), being the n_steps the days of avaiable data and
+        channels the number of variables used as target.
+    times : pd.DatetimeIndex
+        Index that contains the dates of the data.
+        
+    '''
+    
+    df = pd.read_csv(path, index_col=0, parse_dates=['datetime'])
+    
+    df['e'] = e(df['tdew'])
+    
+    prec_daily = df['prec'].resample('D').sum()
+
+    tmax_daily = df['temp'].resample('D').max()
+    tmin_daily = df['temp'].resample('D').min()
+    tmean_daily = df['temp'].resample('D').mean()
+    tamp_daily = tmax_daily - tmin_daily
+    
+    emax_daily = df['e'].resample('D').max()
+    emin_daily = df['e'].resample('D').min()
+    emean_daily = df['e'].resample('D').mean()
+    
+    yearly_cos = np.cos(2 * np.pi * np.clip((prec_daily.index.day_of_year - 1), 0, 364) / 365)
+    yearly_sin = np.sin(2 * np.pi * np.clip((prec_daily.index.day_of_year - 1), 0, 364) / 365)
+    
+    inputs = np.stack([tmax_daily, tmin_daily, tmean_daily, tamp_daily, emax_daily, emin_daily, emean_daily, yearly_cos, yearly_sin], axis=-1)
+    outputs = prec_daily.values[:, None]
+    times = prec_daily.index
+
+    return inputs, outputs, times
+
+
+def load_era5(path, clat, clon):
+    '''
+    Load era5 data from .nc files. Syncing the existing times into one
+    single grid.
+    
+    Returns grid, lats and lons that will be used to load and interpolate
+    goes data and times of each grid step.
+    
+    Parameters
+    ----------
+    path : str
+        placeholder for a string with the path to the era5 data
+    clat : float
+        central float to the station latitude coordinate.
+    clon : float
+        central float to the station longitude coordinate.
+        
+    Returns
+    -------
+    : np.ndarray
+        Concatenation of the grids, on the channel dimension.
+    target_lats : np.ndarray
+        Latitudes where goes will be interpolated.
+    target_lons : np.ndarray
+        Longitudes where goes will be interpolated.
+    : pd.DatetimeIndex
+        Index containing the times where the data was gathered.
+        
+    '''
+    
+    vars = ['T', 'Td', 'msl']
+    year = 2018 # use only 2018 - goes has only this one xD
+    
+    time = None
+    islice = None
+    jslice = None
+    target_lats = None
+    target_lons = None
+    
+    grids = []
+    for var in vars:
+        idx = 12 # index where year 2018 is stored
+        nc_file = nc.Dataset(f'D:\\era5\\{var}_%d.nc'%idx)
+        load_var = list(set(nc_file.variables).difference(set(nc_file.dimensions)))[0]
+        
+        if time is None:
+            time = ncHelper.load_time(nc_file['time'])
+            lats, lons = ncHelper.get_lats_lons(nc_file)
+            i = np.argmin(np.abs(lats - clat))
+            j = np.argmin(np.abs(lons - clon))
+            
+            islice = slice(i - 90, i + 91)
+            jslice = slice(j - 90, j + 91)
+            
+            target_lats = lats[islice][::-1]
+            target_lons = lons[jslice]
+        
+        local_time = ncHelper.load_time(nc_file['time'])
+        
+        _, _, local_indices = np.intersect1d(time, local_time, return_indices=True)
+        
+        grid = nc_file[load_var][local_indices, islice, jslice][:, ::-1, :, None]
+        grids.append(grid)
+        
+    return np.concatenate(grids, axis=-1), target_lats, target_lons, pd.Index(time)
+
+
+def load_goes(path, tlats, tlons):
+    '''
+    Load goes data from .nc files. Using tlats and tlons to interpolate
+    the grid of each file to the preferred dimension.
+    
+    Returns grid and time of valid found datetimes.
+    
+    Parameters
+    ----------
+    path : str
+        placeholder for a string with the path to the era5 data
+    tlats : float
+        Latitudes used in the interpolation.
+    tlons : float
+        Longitudes used in the interpolation.
+        
+    Returns
+    -------
+    : np.ndarray
+        Concatenation of the grids, on the channel dimension.
+    : pd.DatetimeIndex
+        Index containing the times where the data was gathered.
+        
+    '''
+    
+    base = 'D:\\cptec\\S10635346_2018%m%d%H00.nc'
+    datetimes = pd.date_range('2018-01-01T00', '2018-12-31T23', freq='6H')
+    
+    valid_times = []
+    grids = []
+    for datetime in tqdm(datetimes):
+        if glob.glob(datetime.strftime(base)) != []:
+            nc_file = nc.Dataset(datetime.strftime(base))
+            lats, lons = ncHelper.get_lats_lons(nc_file)
+            
+            grids.append(RectBivariateSpline(lats, lons, nc_file['Band1'][:])(tlats, tlons)[None, ..., None])
+            valid_times.append(datetime)
+        
+    return np.concatenate(grids, axis=0), pd.Index(valid_times)
+        
+
+
+def load_data_v3(path='data/sp_mirante.csv', era5_path='', goes_path=''):
+    '''
+    Load data from one .csv, take those variables and prepare train and test sets.
+    
+    Returns arrays with the information needed for training and testing,
+    and additional information about the samples gathered.
+    
+    Parameters
+    ----------
+    path : str
+        String that contains the path to the .csv file to be used.
+        
+    Returns
+    -------
+    x_train : np.ndarray
+        Array with samples of the input data (samples, 10, 9) for
+        training
+    [y_train, y_train_bin] : list
+        List of np.ndarray's with the target data (samples, 13, 1) and
+        (samples, 13, 2) for training
+    x_test : np.ndarray
+        Array with samples of the input data (samples, 10, 9) for
+        testing
+    [y_test, y_test_bin] : list
+        List of np.ndarray's with the target data (samples, 13, 1) and
+        (samples, 13, 2) for testing
+    weights : list
+        List of weights for balancing classes
+    times_train : np.ndarray
+        Array with head time of each sample of the training set
+    times_test : np.ndarray
+        Array with head time of each sample of the testing set
+    
+    '''
+    
+    inputs, outputs, times = load_base_v3(path)
+    
+    center_lat, center_lon = -23.48, -46.63
+    era5_inputs, target_lats, target_lons, era5_times = load_era5(era5_path, center_lat, center_lon) # get (N, 181, 181, c) grid
+    goes_inputs, goes_times = load_goes(goes_path, target_lats, target_lons) # usage of RectBivariateSpline & (M, 181, 181, 1) grid
+    
+    print(era5_inputs.shape, goes_inputs.shape)
+    
+    _, era5_idx, goes_idx = np.intersect1d(era5_times, goes_times, return_indices=True)
+    
+    era5_inputs = era5_inputs[era5_idx]
+    goes_inputs = goes_inputs[goes_idx]
+    
+    HUE 
+    # sync time indices
+    
+    
+    train_mask = (times >= '2007-01') & (times <= '2020-12')
+    test_mask = times >= '2021-01'
+    
+    data_train = (inputs[train_mask], outputs[train_mask])
+    data_test = (inputs[test_mask], outputs[test_mask])
+    times_train, times_test = times[train_mask], times[test_mask]
+    times_train = times_train[:-12]
+    times_test = times_test[:-12]
+    
+    x_train = []
+    y_train = []
+    for i in range(data_train[0].shape[0] - 9 - 3):
+        in_slice = slice(i, i + 10)
+        out_slice = slice(i, i + 10 + 3)
+        x_train.append(data_train[0][in_slice])
+        y_train.append(data_train[1][out_slice])
+    
+    x_test = []
+    y_test = []
+    for i in range(data_test[0].shape[0] - 9 - 3):
+        in_slice = slice(i, i + 10)
+        out_slice = slice(i, i + 10 + 3)
+        x_test.append(data_test[0][in_slice])
+        y_test.append(data_test[1][out_slice])
+    
+    x_train, y_train, x_test, y_test = list(map(np.array, [x_train, y_train, x_test, y_test]))
+    
+    # filter nan
+    valid_train = np.min(~np.isnan(x_train), axis=(1, 2))
+    x_train = x_train[valid_train]
+    y_train = y_train[valid_train]
+    times_train = times_train[valid_train]
+    
+    valid_test = np.min(~np.isnan(x_test), axis=(1, 2))
+    x_test = x_test[valid_test]
+    y_test = y_test[valid_test]
+    times_test = times_test[valid_test]
+    
+    # bin & log
+    y_train_bin = np.int32(y_train > 1) # > 1mm
+    y_test_bin = np.int32(y_test > 1)
+    
+    y_train = np.log(y_train + 1)
+    y_test = np.log(y_test + 1)
+    
+    weights = [
+        y_train_bin.shape[0] / (2 * np.sum(y_train_bin == 0)),
+        y_train_bin.shape[0] / (2 * np.sum(y_train_bin == 1)),
+    ]
+    
+    return x_train, [y_train, y_train_bin], x_test, [y_test, y_test_bin], weights, times_train, times_test
+    
+
+if __name__ == '__main__':
+    res = load_data_v3('../data/sp_mirante.csv')
+    
+    print(res)
--- src/models.py
+++ src/models.py
@@ -4,14 +4,15 @@ from tensorflow.keras import layers, models
 
 def gen_model_v1(input_shape=(None, 3 + 2)):
     '''
-    Basic hard-coded model for 3 step forecast, first try using an tanh activation
-    before the usage of LSTM, as to simulate how the LSTMCell sees a information
-    input, but such assumption was incorrect due to the usage of sigmoid internally
-    the LSTMCell which reduces the value of 'raw' tanh, rendering the artificial
-    tanh input wrong.
+    Basic hard-coded model for 3 step forecast, first try using an tanh 
+    activation before the usage of LSTM, as to simulate how the LSTMCell sees
+    a information input, but such assumption was incorrect due to the usage of 
+    sigmoid internally the LSTMCell which reduces the value of 'raw' tanh, 
+    rendering the artificial tanh input wrong.
     
     Also, when training both value and probabilities, the backpropagation for
-    the probabilities should be restricted only to the last Dense trainable_variables.
+    the probabilities should be restricted only to the last Dense
+    trainable_variables.
     
     Returns model that takes (N, channels) and outputs [(N + 3, 1), (N + 3, 2)]
     which are values and categorical, so it can be checked the amount of rain
@@ -20,8 +21,8 @@ def gen_model_v1(input_shape=(None, 3 + 2)):
     Parameters
     ----------
     input_shape : tuple
-        Tuple indicating the number os timesteps and number of channels the model
-        will be built with.
+        Tuple indicating the number os timesteps and number of channels the
+        model will be built with.
         
     Returns
     -------
@@ -62,7 +63,29 @@ def gen_model_v1(input_shape=(None, 3 + 2)):
     
 def gen_model_v2(input_shape=(None, 3 + 2)):
     '''
-
+    A bit less basic hard-coded model for 3 step forecast, correcting the
+    tanh trial for a LSTM layer that will create values similar to the next
+    RNN output, therefore helping the forecast used by the 3 LSTMCell's.
+    
+    Another layer of Dense was added before the final outputs to help in the
+    decision making, just in case the raw LSTM isn't enough.
+    
+    Returns model that takes (N, channels) and outputs [(N + 3, 1), (N + 3, 2)]
+    which are values and categorical, so it can be checked the amount of rain
+    and probability of rainfall on those days.
+    
+    Parameters
+    ----------
+    input_shape : tuple
+        Tuple indicating the number os timesteps and number of channels the
+        model will be built with.
+        
+    Returns
+    -------
+    model : tensorflow.keras.models.Model
+        A keras Model that takes the input layer with the given input_shape and
+        two outputs, the first is the value, without activation and the second
+        is a for categorical output with logits, so also without activation.
         
     '''
     
@@ -115,3 +138,154 @@ def gen_model_v2(input_shape=(None, 3 + 2)):
 
     model = models.Model(_in, [out_value, out_prob])
     return model
+    
+    
+def gen_model_v3(input_shape=(None, 3 + 2), era5_shape=(181, 181, 1), goes_shape=(181, 181, 1)):
+    '''
+    Need to reduce the goes resolution to a usable (memory-wise, like 0.25 degree), making use of a (181, 181) image
+    and a kernel of 61, to simulate one day of transportation or information, may not be ideal.
+    
+    The kernel size comes from 800km (@10m/s) divided by average 30km per pixel, giving one day of travel of the
+    signal in a horizontal radius. Not considering corners of the kernel that will have greater distances.
+    
+    Both era5 and goes will not have time, they'll only be used at the start of the forecast steps.
+    
+    Parameters
+    ----------
+    input_shape : tuple
+        Tuple indicating the number os timesteps and number of channels the
+        model will be built with.
+    era5_shape : tuple
+        Tuple indicating size of era5 grid data and channels.
+    goes_shape : tuple
+        Tuple indicating size of goes grid data and channels.
+        
+    Returns
+    -------
+    model : tensorflow.keras.models.Model
+        A keras Model that takes the input layer with the given input_shape and
+        two outputs, the first is the value, without activation and the second
+        is a for categorical output with logits, so also without activation.    
+        
+    '''
+    
+    # era5 conv layers
+    era5_conv = models.Sequential(layers.SeparableConv2D(64, 61, 1, 'same', input_shape=(*era5_shape[:-1], 64)))
+    era5_conv.add(layers.BatchNormalization())
+    era5_conv.add(layers.Activation('relu'))
+    # goes conv layers
+    goes_conv = models.Sequential(layers.SeparableConv2D(64, 61, 1, 'same', input_shape=(*goes_shape[:-1], 64)))
+    goes_conv.add(layers.BatchNormalization())
+    goes_conv.add(layers.Activation('relu'))
+    # before lstmcell usage
+    get_center = layers.Lambda(lambda x: x[:, tf.shape(x)[1] // 2, tf.shape(x)[2] // 2, :]) # (b, i, j, c) -> (b, c)
+    decision_attention = layers.MultiHeadAttention(4, 4, attention_axes=(1,))
+    # lstm forecast stuff
+    lstm_cell = layers.LSTMCell(64)
+    lstm_rnn  = layers.RNN(lstm_cell, return_sequences=True, return_state=True)
+
+    _in = layers.Input(input_shape)
+    c = layers.BatchNormalization()(_in)
+    
+    # era5
+    _in_era5 = layers.Input(era5_shape)
+    era5_info = layers.BatchNormalization()(_in_era5)
+    
+    era5_info = layers.Dense(32)(era5_info)
+    era5_info = layers.BatchNormalization()(era5_info)
+    era5_info = layers.Activation('relu')(era5_info)
+    
+    era5_info = layers.Dense(64)(era5_info)
+    era5_info = layers.BatchNormalization()(era5_info)
+    era5_info = layers.Activation('relu')(era5_info)
+
+    # goes
+    _in_goes = layers.Input(goes_shape)
+    goes_info = layers.BatchNormalization()(_in_goes)
+    
+    goes_info = layers.Dense(32)(goes_info)
+    goes_info = layers.BatchNormalization()(goes_info)
+    goes_info = layers.Activation('relu')(goes_info)
+    
+    goes_info = layers.Dense(64)(goes_info)
+    goes_info = layers.BatchNormalization()(goes_info)
+    goes_info = layers.Activation('relu')(goes_info)
+    
+    # abstração inicial - "Xception-like" - original com Conv2D e SeparableConv2D
+    c = layers.Dense(32)(c)
+    c = layers.BatchNormalization()(c)
+    c = layers.Activation('relu')(c)
+
+    c = layers.Dense(64)(c)
+    c = layers.BatchNormalization()(c)
+    c = layers.Activation('relu')(c)
+
+    for _ in range(8):
+        res = c
+        
+        for _ in range(3):
+            c = layers.Activation('relu')(c)
+            c = layers.Dense(64)(c)
+            c = layers.BatchNormalization()(c)
+            
+        c = layers.Add()([c, res])
+
+    # LSTM convencional, para a próxima camada conseguir usar os inputs
+    # de forma consistente "tanh * sigmoid" nos steps futuros
+    c = layers.LSTM(64, return_sequences=True)(c)
+    out0, *state_warm = lstm_rnn(c)
+
+    last_out = out0[:, -1]
+    last_state = state_warm
+    pred_outs = []
+    for _ in range(3):
+        era5_info = era5_conv(era5_info)
+        era5_center = get_center(era5_info)
+        
+        goes_info = era5_conv(goes_info)
+        goes_center = get_center(goes_info)
+        
+        space_info = layers.Concatenate()([last_out, era5_center, goes_center])
+        
+        decided_space_info = decision_attention(last_out, space_info)
+        decided_space_info = layers.Add()([last_out, decided_space_info])
+        decided_space_info = layers.BatchNormalization()(decided_space_info)
+        decided_space_info = layers.Activation('tanh')(decided_space_info)
+        # this breaks the proposal of tanh * sigmoid from the last LSTM
+        
+        last_out, last_state = lstm_cell(decided_space_info, states=last_state)
+        pred_outs.append(last_out)
+
+    pred_outs = tf.stack(pred_outs, axis=1)
+    outs = layers.Concatenate(axis=1)([out0, pred_outs])
+
+    out_value = layers.Dense(128)(outs)
+    out_value = layers.BatchNormalization()(out_value)
+    out_value = layers.Activation('relu')(out_value)
+    out_value = layers.Dense(1, name='value')(out_value)
+    
+    out_prob = layers.Dense(128)(outs)
+    out_prob = layers.BatchNormalization()(out_prob)
+    out_prob = layers.Activation('relu')(out_prob)
+    out_prob  = layers.Dense(2, name='prob')(out_prob)
+
+    model = models.Model([_in, _in_era5, _in_goes], [out_value, out_prob])
+    return model
+    
+    
+if __name__ == '__main__': # testing only
+    import numpy as np
+    
+    a = np.random.rand(32, 10, 9)
+    b = np.random.rand(32, 181, 181, 5)
+    c = np.random.rand(32, 181, 181, 1)
+    
+    d = np.random.rand(32, 13, 1)
+    e = np.random.randint(2, size=(32, 13, 1))
+    
+    model = gen_model_v3((None, 9), (181, 181, 5), (181, 181, 1))
+    model.compile('adam', ['mse', 'sparse_categorical_crossentropy'])
+    
+    model.fit([a, b, c], [d, e], epochs=1) # worked :D
+    # res = model.predict([a, b, c])
+    # print(res[0].shape, res[1].shape) # worked :D
